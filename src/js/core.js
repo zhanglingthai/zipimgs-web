595af69c02c29997d9a9bd00ca9f2d56b0992947
zhangling <zhanglingthai@gmail.com>
2022-07-17 20:56:59 +0700

    dev

--- src/js/core.js
+++ src/js/core.js
@@ -2,14 +2,39 @@
 
 Dropzone.autoDiscover = false;
 
-var dropz = new Dropzone('div.dropzone', {
+var dropz = new Dropzone('div#dropzone', {
     url: 'https://fundscat.com/api/upload',
-    autoProcessQueue: true,// 如果为false，文件将被添加到队列中，但不会自动处理队列。
-    uploadMultiple: true, // 是否在一个请求中发送多个文件。
     parallelUploads: 32, // 并行处理多少个文件上传
-    maxFiles: 32, // 用于限制此Dropzone将处理的最大文件数
     maxFilesize: 5,
+    paramName: 'file',
+    uploadMultiple: true, // 是否在一个请求中发送多个文件。
+    addRemoveLinks: false,//每个预览添加删除按钮
+    previewsContainer: document.querySelector('.resultbox ul'),
+    previewTemplate: `
+    <li class=" clearfix dz-preview dz-file-preview">
+        <div class="left">
+            <img data-dz-thumbnail />
+            <div class="filename" data-dz-name></div>
+            <div class="uploadsize" data-dz-size></div>
+        </div>
+        <div class="center">
+            <div class="progress dz-progress">
+                <span class="dz-upload" data-dz-uploadprogress></span>
+            </div>
+            <div class="dz-success-mark"><span>✔</span></div>
+            <div class="dz-error-mark"><span>✘</span></div>
+            <div class="outputsize">666kb</div>
+        </div>
+        <div class="right">
+            <div><a href="">download</a></div>
+            <div class="proportion">-80%</div>
+            <div class="dz-error-message"><span data-dz-errormessage></span></div>
+        </div>
+    </li>
+    `,
+    maxFiles: 32, // 用于限制此Dropzone将处理的最大文件数
     acceptedFiles: ".jpg,.gif,.png,.svg",
+    autoProcessQueue: true,// 如果为false，文件将被添加到队列中，但不会自动处理队列。
     dictDefaultMessage: `
     <div class="icon"><img src="./imgs/upload-solid.svg"></div>
     <div class="prompt">
@@ -17,11 +42,11 @@ var dropz = new Dropzone('div.dropzone', {
         <p>单次限制 32张 X 5MB</p>
     </div>
     `,
-    previewTemplate:document.querySelector('.resultbox').innerHTML,
     dictFallbackMessage: "你的浏览器不支持拖拉文件来上传",
-    dictInvalidFileType:"文件内包含不支持的格式",
+    dictInvalidFileType: "文件内包含不支持的格式",
     dictMaxFilesExceeded: "文件数量过多",
     dictFileTooBig: "可添加的最大文件大小为{{maxFilesize}}Mb，当前文件大小为{{filesize}}Mb ",
+    dictResponseError: "上传失败：{{statusCode}}",
     init: function () { // dropzone初始化时调用您可以在此处添加事件侦听器
         var myDropzone = this;
         this.on("addedfile", function (file) {
